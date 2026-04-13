314beaf8a6a5c10f3ef35daee95f8be179346c47
AbhishekLayek <abhisheklayek57@gmail.com>
2026-04-13 23:01:14 +0530

    Java 10 Features

---
+++ Java10/LocalVariableTypeInference.java
@@ -0,0 +1,27 @@
+/*
+ * Local Variable Type Inference: Java 10 allows us to use var keyword to declare local variable with an initializer. The compiler will infer the type from the assigned value.
+ */
+
+package Java10;
+
+import java.util.Arrays;
+
+public class LocalVariableTypeInference {
+	public static void main(String[] args) {
+		
+		var v = 10;
+		System.out.println(v); // 10
+		
+		var arr = new int[5];
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+		arr[3] = 4;
+		arr[4] = 5;
+		
+		System.out.println(Arrays.toString(arr)); // [1, 2, 3, 4, 5]
+		
+		var obj = new LocalVariableTypeInference();
+		System.out.println(obj); // Java10.LocalVariableTypeInference@54bedef2
+	}
+}
