90ad4e9b8c369337a7d8feb2535189418c968836
AbhishekLayek <abhisheklayek57@gmail.com>
2026-04-14 22:49:34 +0530

    Java 15 Features

---
+++ Java15/TextBlock.java
@@ -0,0 +1,22 @@
+package Java15;
+
+public class TextBlock {
+	public static void main(String[] args) {
+		
+		String text = """
+					  Hello Everyone,
+					  This Is Java Text Block.
+					  It Supports Multiple Line.
+					  """;
+		
+		System.out.println(text);
+	}
+}
+
+/*
+ * Output:
+ * 
+ * Hello Everyone,
+ * This Is Java Text Block.
+ * It Supports Multiple Line.
+ */
