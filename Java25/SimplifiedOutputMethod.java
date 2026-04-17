73abf4a744888e13677601faf131723b69696849
AbhishekLayek <abhisheklayek57@gmail.com>
2026-04-17 23:30:59 +0530

    Simplified Console Output Method

---
+++ Java25/SimplifiedOutputMethod.java
@@ -0,0 +1,14 @@
+/*
+ * Java 25 provides new IO class which belongs to java.lang package, which consists simple static methods for console outputs and inputs.
+ */
+
+package Java25;
+
+import static java.lang.IO.*; // Import java.lang.IO.* as static.
+
+public class SimplifiedOutputMethod {
+	void main() {
+		IO.println("Simplified Print Statement"); // Simplified Print Statement
+		println("Java25"); // Java25
+	}
+}
