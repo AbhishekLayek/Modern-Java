3fd4416ce212670bcad0e34074e6333e4b276246
AbhishekLayek <abhisheklayek57@gmail.com>
2026-04-18 21:57:46 +0530

    Simplified Console Input Method

---
+++ Java25/SimplifiedInputMethod.java
@@ -0,0 +1,17 @@
+package Java25;
+
+import static java.lang.IO.*;
+
+public class SimplifiedInputMethod {
+	void main() {
+		var name = readln("Enter Your Name: ");
+		println("Name: " + name);
+	}
+}
+
+/*
+ * Output:
+ * 
+ * Enter Your Name: Abhishek
+ * Name: Abhishek
+ */
